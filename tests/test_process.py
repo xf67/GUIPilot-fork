54ef6913f074826e702f5e1799c4a5957e139b35
xf67 <xf6767@gmail.com>
2025-12-27 02:32:25 +0800

    fix 1st test

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,12 +1,23 @@
+import numpy as np
+
+from guipilot.entities.constants import Bbox
 from guipilot.entities.process import Process
-from guipilot.entities.screen import Screen  # 假设 Screen 类在这个路径下
+from guipilot.entities.screen import Screen
+from guipilot.entities.widget import Widget, WidgetType
 
 
 def test_add_screen():
     process = Process()
-    screen = Screen()  # 创建一个 Screen 实例
+    image = np.zeros((100, 100, 3), dtype=np.uint8)
 
-    process.add(screen)  # 调用 add 方法
+    bbox = Bbox(10, 10, 50, 50)
+    widget = Widget(type=WidgetType.TEXT_BUTTON, bbox=bbox)
+
+    widgets = {0: widget}
+
+    screen = Screen(image=image, widgets=widgets)
+
+    process.add(screen)
 
     assert len(process.screens) == 1  # 确保 screens 列表中有一个元素
     assert process.screens[0] is screen  # 确保添加的元素是我们创建的 screen 实例
